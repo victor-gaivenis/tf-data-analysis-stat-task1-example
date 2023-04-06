e78698bfa87a5ee4d1c3b0f0fb72c403e984e9d8
victor-gaivenis <129993733+victor-gaivenis@users.noreply.github.com>
2023-04-07 00:14:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,18 +10,4 @@ def solution(x: np.array) -> float:
     # Не меняйте название функции и её аргументы
     t = 13 
     n = len(x) 
-    v0 = x 
-    v1 + x = np.random.normal(-43, np.exp(1), size=n) 
-    d = np.trapz(v1, dx=t) 
-    a = 2*(d - v0*t*n)/(t**2 * n) 
-    mse = ((pd.Series(a) - 2)**2).mean() 
-    if n == 1000 and mse <= 0.0000288:
-        return x.mean() + 1
-    elif n == 1000 and mse <= 0.00000288:
-        return x.mean() + 1
-    elif n == 100 and mse <= 0.00000808:
-        return x.mean() + 1
-    elif n == 10 and mse <= 0.0000323:
-        return x.mean() + 1
-    else:
-    return x.mean() # Ваш ответ
+    return x.mean()/(13**2) # Ваш ответ
